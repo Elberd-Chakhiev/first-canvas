32c60abc1cd769e02f8b119b4eea29aae37be38d
Elberd Chahkiev <takututu95@gmail.com>
2022-06-29 01:00:36 +0300

    add circle

--- script.js
+++ script.js
@@ -13,14 +13,13 @@ resize();
 
 const settings = 
 {
-    opacity: 0.005,
+    opacity: 0.1,
     count: 100, 
     fps: 60,
     color: 'hsl(hue, 100%, 50%)', 
-    hue: 10,
+    hue: Number(document.getElementById('hue').value),
     divisionSpeed: 100,
 }
-
 const init = () => 
 {
  window.requestAnimationFrame(init);
@@ -29,16 +28,28 @@ const init = () =>
 
 let pointerX = 0;
 let pointerY = 0;
+let count = 0;
 const findCurPos = e => 
 {
     pointerX = e.clientX;
     pointerY = e.clientY;
 }
-window.addEventListener('mousemove', findCurPos)
+
+
+document.getElementById('set').addEventListener('click', () => {settings.hue = Number(document.getElementById('hue').value)});
+window.addEventListener('mousemove', findCurPos);
 
 
 const Step = () => 
 {
+
+    let circle = new Path2D();
+    circle.arc(pointerX, pointerY, 25, 0, 2 * Math.PI);
+    ctx.fillRect( pointerX, pointerY, 10, 10);
+
+    circle.moveTo(pointerX, pointerY);
+
+    
 
     for(let i = 0; i < settings.count; i++)
     {
@@ -48,7 +59,8 @@ const Step = () =>
         let fillColor = settings.color.replace('hue', settings.hue);
 
         ctx.fillStyle = fillColor;
-        ctx.fillRect( pointerX, pointerY, 10, 10);
+
+        ctx.fill(circle);
     }
     ctx.fillStyle = `rgba(255,255,255, ${+ settings.opacity})`;
     ctx.fillRect(0,0, width, height);
